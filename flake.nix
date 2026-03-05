2d3266b0d212578a08956782ea0b372476a47f49
yuzujr <15568103056@163.com>
2026-03-05 23:40:48 +0800

    fix: dev shell now support clangd

--- flake.nix
+++ flake.nix
@@ -2,21 +2,23 @@
   description = "ani2xcursor — convert Windows animated cursors to Xcursor themes";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      pkgs   = nixpkgs.legacyPackages.${system};
-    in {
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
 
       packages.${system}.default = pkgs.stdenv.mkDerivation {
-        pname   = "ani2xcursor";
+        pname = "ani2xcursor";
         version = "1.0.0";
-        src     = ./.;
+        src = ./.;
 
         nativeBuildInputs = with pkgs; [
           clang
           pkg-config
-          gettext      # msgfmt for compiling translations
+          gettext # msgfmt for compiling translations
         ];
         buildInputs = with pkgs; [
           spdlog
@@ -37,18 +39,17 @@
 
         meta = {
           description = "Convert Windows animated cursors (.ani/.cur) to Xcursor themes";
-          homepage    = "https://github.com/yuzujr/ani2xcursor";
-          license     = pkgs.lib.licenses.mit;
-          platforms   = pkgs.lib.platforms.linux;
+          homepage = "https://github.com/yuzujr/ani2xcursor";
+          license = pkgs.lib.licenses.mit;
+          platforms = pkgs.lib.platforms.linux;
           mainProgram = "ani2xcursor";
         };
       };
 
-      devShells.${system}.default = pkgs.mkShell {
+      devShells.${system}.default = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
         packages = with pkgs; [
           xmake
           pkg-config
-          clang
           clang-tools
           gettext
           gdb
